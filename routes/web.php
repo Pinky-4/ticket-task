c6da63cbf1c935da22b6d0324813ead8bb93acb7
Pinky-4 <patelpinky075@gmail.com>
2023-10-12 03:07:22 +0530

    - add readMe and home route change.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TicketController;
 
 /*
@@ -21,10 +21,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 // Ticket Routes
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [TicketController::class, 'index'])->name('home');
     Route::name('ticket.')->prefix('ticket')->group(function () {
         Route::get('/list', [TicketController::class, 'index'])->name('list');
         Route::post('/datatable-list', [TicketController::class, 'dataTableList'])->name('datatable.list');
